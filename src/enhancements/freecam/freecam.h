73b4d949bc042b180a19e513f9d76d6c2d87f0d9
MegaMech <inkstudios1@hotmail.com>
2025-01-07 16:43:36 -0700

    Target player follows player and fix

--- src/enhancements/freecam/freecam.h
+++ src/enhancements/freecam/freecam.h
@@ -16,6 +16,7 @@ void freecam_keyboard_manager(Camera*, Vec3f);
 extern f32 gFreecamSpeed;
 extern f32 gFreecamSpeedMultiplier;
 extern f32 gFreecamRotateSmoothingFactor;
+extern f32 gFreecamRotateFollowFactor;
 extern u32 fRankIndex;
 extern u32 gFreecamControllerType;
 
